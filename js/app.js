a23ab7bd99b9772b4c7fd5b54172264fc79a7afc
AnthonyCorrado <amcstudios@yahoo.com>
2015-07-11 18:58:19 -0700

    building out basic routing and components

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.route("about");
+  this.route("collections");
+  this.resource("exhibits", function() {
+    this.resource("exhibit", {path: "/:exhibit_id"});
+  });
+  this.route("notes");
 });
 
-App.IndexRoute = Ember.Route.extend({
+// collection route
+App.CollectionsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return [
+      {
+        title: "Samoca Photography Collections",
+        copy: "The Photography Gallery exhibits over 2,000 rotating examples from the permanent collection",
+        image: "images/photo.png"
+      },
+      {
+        title: "Samoca Painting Collections",
+        copy: "The Painting Gallery exhibits over 250 rotating examples from all over the world",
+        image: "images/painting.png"
+      },
+      {
+        title: "Samoca Sculpture Collections",
+        copy: "The Sculpture Gallery exhibits over 80 examples from the Roman collection",
+        image: "images/sculpture.png"
+      }
+    ];
   }
+});
+
+App.SingleCollectionComponent = Ember.Component.extend({
+  classNames: "collectionList"
 });
